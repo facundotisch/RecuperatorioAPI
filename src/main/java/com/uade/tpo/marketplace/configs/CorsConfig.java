0c37661d98646412df18268d8177366b10d06fde
Alejo Garcia Bosio <agarciabosio@uade.edu.ar>
2025-11-17 16:27:45 -0300

    Fix Springboot Dashboard

--- src/main/java/com/uade/tpo/marketplace/configs/CorsConfig.java
+++ src/main/java/com/uade/tpo/marketplace/configs/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.uade.tpo.marketplace.config;
+package com.uade.tpo.marketplace.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
